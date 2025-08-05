656039a268a4d0e39ad3fba3145098f6fe0498d5
Sunnatillo <sunnatilloshavkatov@gmail.com>
2025-08-05 15:08:50 +0500

    media3Version 1.7.1 => 1.8.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,8 +12,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.android.tools.build:gradle:8.11.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
@@ -52,7 +52,7 @@ android {
 
 dependencies {
     // Media3
-    val media3Version = "1.7.1"
+    val media3Version = "1.8.0"
     implementation("androidx.media3:media3-ui:$media3Version")
     implementation("androidx.media3:media3-exoplayer:$media3Version")
     implementation("androidx.media3:media3-exoplayer-hls:$media3Version")
